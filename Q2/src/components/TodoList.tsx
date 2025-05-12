6941a67009a8ab260f6f424000ad91b9e50b1d90
theDAOS <sanjusanthosh46@gmail.com>
2025-05-12 15:09:26 +0530

    Q2

--- Q2/src/components/TodoList.tsx
+++ Q2/src/components/TodoList.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { getData, getDataFromLocal } from "../redux/todoSlice";
 
 function TodoList() {
-    const { todo, loading } = useSelector((state: stateType) => state)
+    let { todo, loading } = useSelector((state: stateType) => state)
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -16,6 +16,19 @@ function TodoList() {
         <div>
             {loading && <p>Loading...</p>}
             {!loading && todo.length <= 0 && <p>No Data Found</p>}
+            {!loading && todo.length > 0 && (
+                <div className="p-2 flex gap-2 items-center border-b border-s-stone-900 mb-5 w-fit">
+                    <span className="font-semibold text-lg">FIlter: </span>
+
+                    <select
+                        className="p-1 px-2 border-2 border-stone-900 rounded font-semibold hover:bg-stone-600 hover:text-stone-50"
+                    >
+                        <option value={null}>All</option>
+                        <option value={true}>Completed</option>
+                        <option value={false}>Incomplete</option>
+                    </select>
+                </div>
+            )}
             {todo.map((task: todoType) => <Card task={task} />)}
         </div>
     )
